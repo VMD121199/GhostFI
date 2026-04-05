3c20de24473385212161ccf935ace1233239c396
Minh-Duc VU <minhducvu1211@gmail.com>
2026-04-05 08:38:20 +0200

    fix port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
+    allowedHosts: ['ghostfi.onrender.com']
   },
-  allowedHosts: ['ghostfi.onrender.com']
 })
